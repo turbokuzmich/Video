571d1cc50c5b7fe60e6a796ba9ac882752d9beec
Дмитрий Куртеев <dmitriy@ricky-mackbook.local>
2013-08-28 14:57:40 +0400

    Адаптация под iPad

--- player/js/controller.js
+++ player/js/controller.js
@@ -3,12 +3,16 @@
 	var controller = new Controller;
 
 	var player = videojs('player', {
-		'width': 960,
-		'height': 540,
+		'width': '100%',
+		'height': '100%',
 		'controls': true,
 		'nativeControlsForTouch': false
 	}, function() {
-		this.src('/videos/small.webm');
+		this.src([
+			{ type: "video/mp4", src: "/videos/small.mp4" },
+			{ type: "video/webm", src: "/videos/small.webm" },
+			{ type: "video/ogg", src: "/videos/small.flv" }
+		]);
 	});
 
 
