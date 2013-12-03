87c541fdb46fb91bd64baf7fc132be796f0dd34d
Дмитрий Куртеев <dmitriy@ricky-mackbook.local>
2013-12-03 11:19:08 +0400

    Добавлен отдельный плеер для IE8

--- player/js/controller.js
+++ player/js/controller.js
@@ -1,142 +1,160 @@
 (function($) {
 
-	var controller = new Controller
-	,	menu = new Menu
-	,	player = new Player({
-			'player': {
-				'width': '100%',
-				'height': '100%',
-				'controls': true,
-				'preload': true,
-				'nativeControlsForTouch': false,
+	var player;
 
-				'flash': {
-					'swf': 'js/libz/videojs/video-js.swf'
-				}
-			},
-			'videos': [{
-				'path': 'videos/mrisksP1',
-				'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка',
-				'misc': {
-					'path': 'videos/mrisksG1',
-					'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка'
-				}
-			}, {
-				'path': 'videos/mrisksP2',
-				'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка',
-				'hide': true,
-				'misc': {
-					'path': 'videos/mrisksG2',
-					'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка'
-				}
-			}, {
-				'path': 'videos/mrisksP3',
-				'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка',
-				'hide': true,
-				'misc': {
-					'path': 'videos/mrisksG3',
-					'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка'
-				}
-			}, {
-				'path': 'videos/mrisksP4',
-				'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка',
-				'hide': true,
-				'misc': {
-					'path': 'videos/mrisksG4',
-					'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка'
-				}
-			}, {
-				'path': 'videos/mrisksP5',
-				'name': 'Кредитный риск'
-			}, {
-				'path': 'videos/mrisksP6',
-				'name': 'Модель LGD',
-				'misc': {
-					'path': 'videos/mrisksG6',
-					'name': 'Модель LGD'
-				}
-			}, {
-				'path': 'videos/mrisksP7',
-				'name': 'Стоимость под риском дефолта. Модель EAD'
-			}, {
-				'path': 'videos/mrisksP8',
-				'name': 'Рыночный риск',
-				'misc': {
-					'path': 'videos/mrisksG8',
-					'name': 'Рыночный риск'
-				}
-			}, {
-				'path': 'videos/mrisksP9',
-				'name': 'Надбавка за риск торговой книги',
-				'misc': {
-					'path': 'videos/mrisksG9',
-					'name': 'Надбавка за риск торговой книги'
-				}
-			}, {
-				'path': 'videos/mrisksP10',
-				'name': 'Операционный риск'
-			}, {
-				'path': 'videos/mrisksP11',
-				'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях',
-				'misc': {
-					'path': 'videos/mrisksG11',
-					'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях'
-				}
-			}, {
-				'path': 'videos/mrisksP12',
-				'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях',
-				'hide': true,
-				'misc': {
-					'path': 'videos/mrisksG12',
-					'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях'
-				}
-			}, {
-				'path': 'videos/mrisksP13',
-				'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях',
-				'hide': true,
-				'misc': {
-					'path': 'videos/mrisksG13',
-					'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях'
-				}
-			}, {
-				'path': 'videos/mrisksP14',
-				'name': 'Прочие риски'
-			}, {
-				'path': 'videos/mrisksP15',
-				'name': 'Процессы. На примере корпоративного кредитного риска'
-			}, {
-				'path': 'videos/mrisksP16',
-				'name': 'Процессы. На примере корпоративного кредитного риска',
-				'hide': true
-			}, {
-				'path': 'videos/mrisksP17',
-				'name': 'Активы, взвешенные с учетом риска',
-				'misc': {
-					'path': 'videos/mrisksG17',
-					'name': 'Активы, взвешенные с учетом риска'
-				}
-			}, {
-				'path': 'videos/mrisksP18',
-				'name': 'Расчет экономического капитала'
-			}, {
-				'path': 'videos/mrisksP19',
-				'name': 'Стресс-тестирование',
-				'misc': {
-					'path': 'videos/mrisksG19',
-					'name': 'Стресс-тестирование'
-				}
-			}, {
-				'path': 'videos/mrisksP20',
-				'name': 'Аппетит к риску. Лимиты. Процесс стратегического и бизнес-планирования',
-				'misc': {
-					'path': 'videos/mrisksG20',
-					'name': 'Аппетит к риску. Лимиты. Процесс стратегического и бизнес-планирования'
-				}
-			}, {
-				'path': 'videos/mrisksP21',
-				'name': 'Заключительное слово'
-			}]
-		});
+	if (ie8) {
+		player = {
+			'isFullyWatched': function() {
+				return false;
+			}
+		};
+
+		var flashvars = {}
+		,	params = { "allowScriptAccess": "always", "wmode" : "transparent" }
+		,	attributes = { "id": "player" };
+
+		$('.wrap').remove();
+		$('.ie8-player-container').removeClass('hide');
+		swfobject.embedSWF("player.swf", "ie8-player", "960", "540", "11.0.0", "/js/libz/swfobject/expressInstall.swf", flashvars, params, attributes);
+	} else {
+		var controller = new Controller
+		,	menu = new Menu
+		,	player = new Player({
+				'player': {
+					'width': '100%',
+					'height': '100%',
+					'controls': true,
+					'preload': true,
+					'nativeControlsForTouch': false,
+
+					'flash': {
+						'swf': 'js/libz/videojs/video-js.swf'
+					}
+				},
+				'videos': [{
+					'path': 'video/mrisksP1',
+					'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка',
+					'misc': {
+						'path': 'video/mrisksG1',
+						'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка'
+					}
+				}, {
+					'path': 'video/mrisksP2',
+					'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка',
+					'hide': true,
+					'misc': {
+						'path': 'video/mrisksG2',
+						'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка'
+					}
+				}, {
+					'path': 'video/mrisksP3',
+					'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка',
+					'hide': true,
+					'misc': {
+						'path': 'video/mrisksG3',
+						'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка'
+					}
+				}, {
+					'path': 'video/mrisksP4',
+					'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка',
+					'hide': true,
+					'misc': {
+						'path': 'video/mrisksG4',
+						'name': 'Предпосылки построения системы интегрированного управления рисками Сбербанка'
+					}
+				}, {
+					'path': 'video/mrisksP5',
+					'name': 'Кредитный риск'
+				}, {
+					'path': 'video/mrisksP6',
+					'name': 'Модель LGD',
+					'misc': {
+						'path': 'video/mrisksG6',
+						'name': 'Модель LGD'
+					}
+				}, {
+					'path': 'video/mrisksP7',
+					'name': 'Стоимость под риском дефолта. Модель EAD'
+				}, {
+					'path': 'video/mrisksP8',
+					'name': 'Рыночный риск',
+					'misc': {
+						'path': 'video/mrisksG8',
+						'name': 'Рыночный риск'
+					}
+				}, {
+					'path': 'video/mrisksP9',
+					'name': 'Надбавка за риск торговой книги',
+					'misc': {
+						'path': 'video/mrisksG9',
+						'name': 'Надбавка за риск торговой книги'
+					}
+				}, {
+					'path': 'video/mrisksP10',
+					'name': 'Операционный риск'
+				}, {
+					'path': 'video/mrisksP11',
+					'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях',
+					'misc': {
+						'path': 'video/mrisksG11',
+						'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях'
+					}
+				}, {
+					'path': 'video/mrisksP12',
+					'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях',
+					'hide': true,
+					'misc': {
+						'path': 'video/mrisksG12',
+						'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях'
+					}
+				}, {
+					'path': 'video/mrisksP13',
+					'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях',
+					'hide': true,
+					'misc': {
+						'path': 'video/mrisksG13',
+						'name': 'Внешние и внутренние данные о потерях. Сбор данных о потерях'
+					}
+				}, {
+					'path': 'video/mrisksP14',
+					'name': 'Прочие риски'
+				}, {
+					'path': 'video/mrisksP15',
+					'name': 'Процессы. На примере корпоративного кредитного риска'
+				}, {
+					'path': 'video/mrisksP16',
+					'name': 'Процессы. На примере корпоративного кредитного риска',
+					'hide': true
+				}, {
+					'path': 'video/mrisksP17',
+					'name': 'Активы, взвешенные с учетом риска',
+					'misc': {
+						'path': 'video/mrisksG17',
+						'name': 'Активы, взвешенные с учетом риска'
+					}
+				}, {
+					'path': 'video/mrisksP18',
+					'name': 'Расчет экономического капитала'
+				}, {
+					'path': 'video/mrisksP19',
+					'name': 'Стресс-тестирование',
+					'misc': {
+						'path': 'video/mrisksG19',
+						'name': 'Стресс-тестирование'
+					}
+				}, {
+					'path': 'video/mrisksP20',
+					'name': 'Аппетит к риску. Лимиты. Процесс стратегического и бизнес-планирования',
+					'misc': {
+						'path': 'video/mrisksG20',
+						'name': 'Аппетит к риску. Лимиты. Процесс стратегического и бизнес-планирования'
+					}
+				}, {
+					'path': 'video/mrisksP21',
+					'name': 'Заключительное слово'
+				}]
+			});
+	};
 	
     pipwerks.SCORM.version = '1.2';
     var unloaded = false;
